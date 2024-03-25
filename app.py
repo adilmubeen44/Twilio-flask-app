c74beb1181ea2f88c31505d3386515a61d3859be
adilmubeen44 <163404917+adilmubeen44@users.noreply.github.com>
2024-03-25 17:09:49 +0500

    Update app.py
    updated

--- app.py
+++ app.py
@@ -141,6 +141,11 @@ def voice():
     response.hangup()
     return str(response)
 
+@app.route('/health')
+def health_check():
+    return 'OK', 200
+
+
 if __name__ == '__main__':
     prompt = read_prompt_from_csv(prompt_file_path)
     # Get the PORT environment variable, default to 8000 if not set
